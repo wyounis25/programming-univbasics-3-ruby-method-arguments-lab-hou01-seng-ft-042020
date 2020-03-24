65ac081786b894143a1b8ac4e6f40bb7e6aa620d
walid younis <walid.younis.90@gmail.com>
2020-03-24 16:57:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,8 +2,6 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-def introduction
-end 
 
 def introduction(name)
   puts "Hi, my name is #{name}."
