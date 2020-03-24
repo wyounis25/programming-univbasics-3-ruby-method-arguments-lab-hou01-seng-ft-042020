91c2f47843c94313bfbb7a5f100a8c2d25859d8d
walid younis <walid.younis.90@gmail.com>
2020-03-24 16:59:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,8 +7,8 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
-def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I am learning ot program in #{language}."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name,language = "ruby")
